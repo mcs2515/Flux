4b40de7ad4b4c8c49e793f561ced889f4c214310
Megan Smith <mcs2515@rit.edu>
2018-02-01 09:36:27 -0500

    first commit

---
+++ Assets/Scripts/GyroControls.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroControls : MonoBehaviour {
+
+    private bool gyroEnabled;
+    private Gyroscope gyro;
+    private GameObject cameraContainer;
+    private Quaternion rotation;
+
+    private void Start()
+    {
+        cameraContainer = new GameObject("Camera Container");
+        cameraContainer.transform.position = transform.position;
+        transform.SetParent(cameraContainer.transform);
+        gyroEnabled = EnableGryo();
+    }
+
+    private bool EnableGryo()
+    {
+        if(SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+            cameraContainer.transform.rotation = Quaternion.Euler(90f, 90f, 0f);
+            rotation = new Quaternion(0, 0, 1, 0);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private void Update()
+    {
+        if (gyroEnabled)
+        {
+            transform.localRotation = gyro.attitude * rotation;
+        }
+    }
+
+}
