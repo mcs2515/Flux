f96f437c262d14a5923d85f2702536c3c5e80de0
Amy Pham <amp4129@g.rit.edu>
2018-02-17 00:24:58 -0500

    starting to spawn and animate cube tiles

---
+++ Assets/Scripts/MoveBlocks.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBlocks : MonoBehaviour {
+
+	private Vector3 startMarker;
+	private Vector3 endMarker;
+	public float speed = 1.0F;
+	private float startTime;
+	private float journeyLength;
+
+	private int tileLength;
+	public GameObject model;
+
+	void Start() {
+		tileLength = 5;
+		startTime = Time.time;
+		startMarker = this.transform.position;
+		endMarker = new Vector3 (this.transform.position.x, this.transform.position.y+2.0F, this.transform.position.z);
+		journeyLength = Vector3.Distance(startMarker, endMarker);
+		SpawnTile ();
+	}
+
+	void Update() {
+		float distCovered = (Time.time - startTime) * speed;
+		float fracJourney = distCovered / journeyLength;
+		transform.position = Vector3.Lerp(startMarker, endMarker, fracJourney);
+	}
+
+	private void SpawnTile(){
+		for (int i = 0; i < tileLength; i++) {
+			for (int j = 0; j < tileLength; j++) {
+				if (!(i == 0 && j == 0)) {
+					GameObject block = Instantiate (model, transform.localPosition + transform.forward *i + transform.right*j, transform.rotation, this.transform) as GameObject;
+				}
+			}
+		}
+	}
+}
