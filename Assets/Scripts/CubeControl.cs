aa7505111f126810824e05795a5f3fb5d1c27995
Amy Pham <amp4129@g.rit.edu>
2018-02-18 01:44:50 -0500

    blocks move in turns

---
+++ Assets/Scripts/CubeControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeControl : MonoBehaviour {
+
+	public float speed = 2.0F;
+	private float startTime = 0;
+	private Vector3 startMarker;
+	private Vector3 endMarker;
+	private float journeyLength;
+	private Vector3 currentPos;
+
+	// Use this for initialization
+	void Start () {
+		startMarker = this.transform.position;
+		endMarker = new Vector3 (this.transform.position.x, this.transform.position.y+2.0F, this.transform.position.z);
+		journeyLength = Vector3.Distance(startMarker, endMarker);
+		currentPos = startMarker;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveBlock();
+		transform.position = currentPos;
+	}
+
+	public void assignStart(float t){
+		startTime = t;
+		Debug.Log (startTime + " assigned");
+	}
+
+	void moveBlock(){
+		Debug.Log (startTime);
+		float distCovered = 0;
+		if (Time.time > startTime) {
+			distCovered = (Time.time - startTime) * speed;
+		}
+		float fracJourney = distCovered / journeyLength;
+		currentPos = Vector3.Lerp(startMarker, endMarker, fracJourney);
+	}
+}
---
+++ Assets/Scripts/TileControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileControl : MonoBehaviour {
+	
+	private int tileLength;
+	public GameObject model;
+
+	void Start() {
+		tileLength = 5;
+		//SpawnTile ();
+		for (int i = 0; i < tileLength; i++) {
+			for (int j = 0; j < tileLength; j++) {
+				GameObject block;
+				if (i == 0 && j == 0) {
+					GameObject.Find ("singleCube").AddComponent<CubeControl> ();
+				}
+				else {
+					block = Instantiate (model, transform.localPosition + transform.forward *i + transform.right*j,
+						transform.rotation, this.transform) as GameObject;
+					
+					block.GetComponent<CubeControl> ().assignStart (((float)j)*0.2F);
+				}
+			}
+		}
+	}
+
+	void Update() {
+	}
+
+	private void SpawnTile(){
+		
+	}
+}
