cac8b77c63789cc5ac0fe1ec5c154363f10b9644
mcs2515 <mcs2515@rit.edu>
2018-02-25 15:59:53 -0500

    added sun shader

---
+++ Assets/Scripts/Sun.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun : MonoBehaviour {
+
+    int distance = 490;
+    GameObject player;
+
+// Use this for initialization
+void Start () {
+        player = GameObject.Find("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = (transform.position - player.transform.position).normalized * distance + player.transform.position;
+    }
+}
