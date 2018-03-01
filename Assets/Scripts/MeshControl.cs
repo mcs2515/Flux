daf334cb07e4983e21fabd7d4eef889bccc8847f
Amy Pham <amp4129@g.rit.edu>
2018-02-28 23:38:09 -0500

    added in obstacle with animation

--- Assets/Scripts/MeshControl.cs
+++ Assets/Scripts/MeshControl.cs
@@ -12,8 +12,11 @@ public class MeshControl : MonoBehaviour {
 	bool[] upwardBound;
 	float speed;
 	private Texture2D controllerImg;
-	public Texture2D[] controllerImgs;
-	private int controllerI;
+	public Texture2D[] transIn;
+	public Texture2D[] jumpNoise;
+	private GameObject[] spheres;
+	private int controllerI, sphereTexOffset;
+	bool transitioning;
 
 	// Use this for initialization
 	void Start () {
@@ -22,6 +25,10 @@ public class MeshControl : MonoBehaviour {
 		originalVertices = deformingMesh.vertices;
 		displacedVertices = new Vector3[originalVertices.Length];
 		currentVertices = new Vector3[originalVertices.Length];
+		spheres = new GameObject[Mathf.RoundToInt((float)originalVertices.Length/4)];
+		transitioning = true;
+		sphereTexOffset = 0;
+
 		/*startTimes = new float[originalVertices.Length];
 		upwardBound = new bool[originalVertices.Length];
 		speed = 1.0f;*/
@@ -38,22 +45,63 @@ public class MeshControl : MonoBehaviour {
 		//	upwardBound [i] = true;
 		//}
 
-		Object[] controllerImgObjs = Resources.LoadAll("Noise/", typeof(Texture2D));
-		controllerImgs = new Texture2D[controllerImgObjs.Length];
-		for (int i = 0; i < controllerImgObjs.Length; i++) {
-			controllerImgs [i] = (Texture2D) controllerImgObjs [i];
+		Object[] transInObjs = Resources.LoadAll("jumpTransition/", typeof(Texture2D));
+		Object[] jumpNoiseObjs = Resources.LoadAll("jumpNoise/", typeof(Texture2D));
+		transIn = new Texture2D[transInObjs.Length];
+		for (int i = 0; i < transInObjs.Length; i++) {
+			transIn [i] = (Texture2D) transInObjs [i];
+		}
+		jumpNoise = new Texture2D[jumpNoiseObjs.Length];
+		for (int i = 0; i < jumpNoiseObjs.Length; i++) {
+			jumpNoise [i] = (Texture2D) jumpNoiseObjs [i];
 		}
 		controllerI = 0;
+
+
+		int j = 0;
+		for (int i = 0; i < originalVertices.Length; i++) {
+			spheres [i] = GameObject.CreatePrimitive(PrimitiveType.Plane);
+			//spheres [i] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+			spheres [i].transform.SetParent(transform);
+			spheres [i].transform.position = transform.position + originalVertices[j] + new Vector3(0,10,0);
+			float sphereRad = 0.05f;
+			spheres [i].transform.localScale = new Vector3 (sphereRad, sphereRad, sphereRad);
+
+			spheres [i].GetComponent<Renderer> ().material = GameObject.Find("glowTexHolder").GetComponent<Renderer>().material;
+			spheres [i].GetComponent<Renderer> ().material.SetTextureScale("_MainTex", new Vector2(2f,2f));
+			j += 4;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		for (int i = 0; i < displacedVertices.Length; i++) {
-			UpdateVertexColor(i);
+		if (transitioning) {
+			for (int i = 0; i < displacedVertices.Length; i++) {
+				UpdateVertexColor (transIn, i);
+			}
+			controllerI = (controllerI + 1);
+		} else {
+			for (int i = 0; i < displacedVertices.Length; i++) {
+				UpdateVertexColor (jumpNoise, i);
+			}
+			controllerI = (controllerI + 1) % jumpNoise.Length;
 		}
 		deformingMesh.vertices = currentVertices;
 		deformingMesh.RecalculateNormals();
-		controllerI = (controllerI + 1) % controllerImgs.Length;
+
+		if (controllerI == transIn.Length) {
+			transitioning = false;
+			controllerI = 0;
+		}
+
+		int j = 0;
+		for (int i = 0; i < spheres.Length; i++) {
+			spheres [i].transform.position = transform.position + transform.localScale.x*currentVertices [j];
+			spheres [i].GetComponent<Renderer> ().material.SetTextureOffset ("_MainTex", new Vector2(sphereTexOffset,0));
+			Debug.Log(sphereTexOffset + " " + spheres[i].GetComponent<Renderer>().material);
+			j += 4;
+			sphereTexOffset = (sphereTexOffset + 500) % 15500;
+		}
 	}
 
 	/*void UpdateVertex (int i) {
@@ -84,11 +132,10 @@ public class MeshControl : MonoBehaviour {
 		}
 	}*/
 
-	void UpdateVertexColor(int i){
+	void UpdateVertexColor(Texture2D[] controllerImgs, int i){
 		float tileLength = Mathf.Sqrt (originalVertices.Length);
 		controllerImg = controllerImgs [controllerI];
 		float colorVal = controllerImg.GetPixel (Mathf.RoundToInt ((float)i % tileLength / tileLength * controllerImg.width), Mathf.RoundToInt (Mathf.Floor ((float)i / tileLength) / tileLength * controllerImg.height)).grayscale;
-		currentVertices[i] = originalVertices[i] + new Vector3(0, 5*colorVal, 0);
-		Debug.Log (currentVertices [i]);
+		currentVertices[i] = originalVertices[i] + new Vector3(0, 1*colorVal, 0);
 	}
 }
