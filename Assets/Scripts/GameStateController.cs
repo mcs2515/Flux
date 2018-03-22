7e101f23b184d9991a6cb8cc04f1f76be353b756
mcs2515 <mcs2515@rit.edu>
2018-03-22 08:04:52 -0400

    start of menu codes

---
+++ Assets/Scripts/GameStateController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateController : MonoBehaviour {
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//check to see if player movement is 0
+		//after 5 sec of no movement, bring up pause menu
+		//if resume, go back to game
+		//else bring them to results screen
+
+		//if player reaches the end, bring them to the results screen
+		//if end game and player selects restart,restart game
+		//else bring them to main menu
+	}
+
+	void Resume(){
+	}
+
+	void Pause(){
+	}
+
+	void Quit(){
+	}
+}
