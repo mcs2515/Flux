ff3084a7bb80cb092912ffe328223e8780073654
Amy Pham <amp4129@g.rit.edu>
2018-04-17 11:02:47 -0400

    start block animation on distance trigger

---
+++ Assets/Scripts/AnimController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimController : MonoBehaviour {
+	public Animator anim;
+	private int counter;
+
+	// Use this for initialization
+	void Start () {
+		anim = this.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckStart ();
+	}
+
+	void CheckStart(){
+		if (Vector3.Distance(GameObject.Find ("Player").transform.position, transform.position) < 80.0f) {
+			anim.SetTrigger ("startAnim");
+		}
+	}
+}
