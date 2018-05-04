4ceb9eb6bce2b9f30fb99d56675e9d775a6a5607
mcs2515 <mcs2515@rit.edu>
2018-05-04 19:45:55 -0400

    cleaned up folders and unused assets/mats

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -28,8 +28,6 @@ public class PlayerMove : MonoBehaviour {
 	Vector3 start_rotation = Vector3.zero;
 	Vector3 end_position = Vector3.zero;
 
-	float distance;
-	float slant;
 
     public float speed;
     private float drag;
@@ -53,13 +51,9 @@ public class PlayerMove : MonoBehaviour {
 		playerStateLast = playerState;
 		player = GameObject.Find ("Player");
 
-		end_position = new Vector3(end_obj.transform.position.x, end_obj.transform.position.y, end_obj.transform.position.z);
-
 		start_position = new Vector3 (player.transform.position.x, player.transform.position.y, player.transform.position.z);
 		start_rotation = new Vector3 (player.transform.eulerAngles.x, player.transform.eulerAngles.y, player.transform.eulerAngles.z);
-
-		distance = end_position.z - start_position.z;
-		slant = (end_position.x-start_position.x)/distance;
+		end_position = new Vector3(end_obj.transform.position.x, end_obj.transform.position.y, end_obj.transform.position.z);
 
         controller = gameObject.GetComponent<CharacterController>();    
 		timer = 0f;
@@ -132,7 +126,7 @@ public class PlayerMove : MonoBehaviour {
 
 		//always ground the person
 		moveDir.y -= gravity * Time.deltaTime;
-		moveDir.x = - .11f;
+		moveDir.x = - .11f;//slant
 
 		//slow down player
 		if (moveDir.z > 0) {
--- Assets/Scripts/Sun.cs
+++ Assets/Scripts/Sun.cs
@@ -19,15 +19,9 @@ public class Sun : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		/*Vector3 position = (transform.position - player.transform.position).normalized * distance + player.transform.position;
-
-		//change the x and z axis, keep y the same
-		sun_position = new Vector3 (position.x, transform.position.y, position.z);
-		transform.position = sun_position;*/
 		Vector3 current_position = transform.position;
 
 		current_position.z = player.transform.position.z + distance_z;
-		//current_position.x = player.transform.position.x + distance_x;
 
 		transform.position = current_position;
     }
